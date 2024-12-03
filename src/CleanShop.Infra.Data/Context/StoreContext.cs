79a52a84b591bcbc743cb28ee75318bf1d781495
Luong Dang <dangvluong@gmail.com>
2024-12-03 21:53:43 +0700

    Add Infra.Data and DbContext

---
+++ src/CleanShop.Infra.Data/Context/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using CleanShop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanShop.Infra.Data.Context
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
