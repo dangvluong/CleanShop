05b67f7165371c749100dac9f0b8b25d1d4f46f2
Luong Dang <dangvluong@gmail.com>
2024-12-06 17:47:23 +0700

    Add AutoMapper package

---
+++ src/CleanShop.Application/Commons/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+namespace CleanShop.Application.Commons.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static MapperConfiguration RegisterMappings()
+        {
+            return new MapperConfiguration(cfg =>
+            {
+                // Add mapping profiles here
+            });
+        }
+    }
+}
