866e2a33eba44a44d1e61300ba949a844951f86d
Luong Dang <dangvluong@gmail.com>
2024-12-18 18:15:49 +0700

    Add ProductCard template

--- src/CleanShop.Web/ReactApp/src/app/layout/Header.tsx
+++ src/CleanShop.Web/ReactApp/src/app/layout/Header.tsx
@@ -2,7 +2,7 @@ import { AppBar, Toolbar, Typography } from "@mui/material";
 
 export default function Header() {
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ m: 4 }}>
       <Toolbar>
         <Typography variant="h6">CLEAN-SHOP</Typography>
       </Toolbar>
