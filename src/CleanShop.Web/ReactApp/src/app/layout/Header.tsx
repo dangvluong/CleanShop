71298fad62cc46eb1e1db5f6db3a4c26969fd488
Luong Dang <dangvluong@gmail.com>
2024-12-20 18:10:19 +0700

    Format ProductCard and add DarkMode

--- src/CleanShop.Web/ReactApp/src/app/layout/Header.tsx
+++ src/CleanShop.Web/ReactApp/src/app/layout/Header.tsx
@@ -1,10 +1,18 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
 
-export default function Header() {
+interface Props {
+  switchTheme: (
+    event: React.ChangeEvent<HTMLInputElement>,
+    checked: boolean
+  ) => void;
+}
+
+export default function Header({ switchTheme }: Props) {
   return (
     <AppBar position="static" sx={{ m: 4 }}>
       <Toolbar>
         <Typography variant="h6">CLEAN-SHOP</Typography>
+        <Switch onChange={switchTheme} />
       </Toolbar>
     </AppBar>
   );
