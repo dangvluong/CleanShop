0f5a7461fef5c1dfaa58fb18ae397201803a6f03
Luong Dang <dangvluong@gmail.com>
2024-12-25 17:52:10 +0700

    Implement Product Detail page and using Axios to fetch data from API

--- src/CleanShop.Web/ReactApp/src/features/catalog/ProductDetails.tsx
+++ src/CleanShop.Web/ReactApp/src/features/catalog/ProductDetails.tsx
@@ -1,5 +1,78 @@
-import { Typography } from "@mui/material";
+import {
+  Divider,
+  Grid,
+  Grid2,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { Product } from "../../app/models/product";
 
 export default function ProductDetails() {
-  return <Typography variant="h2">Product details</Typography>;
+  const { id } = useParams<{ id: string }>();
+  const [product, setProduct] = useState<Product | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/products/${id}`)
+      .then((response) => setProduct(response.data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, [id]);
+
+  if (loading) return <h3>Loading...</h3>;
+
+  if (!product) return <h3>Product not found</h3>;
+
+  return (
+    <Grid2 container spacing={6}>
+      <Grid2 size={6}>
+        <img
+          src={product.imageUrl}
+          alt={product.name}
+          style={{ width: "100%" }}
+        />
+      </Grid2>
+      <Grid2 size={6}>
+        <Typography variant="h3">{product.name}</Typography>
+        <Divider sx={{ mb: 2 }} />
+        <Typography variant="h4" color="secondary">
+          ${(product.price / 100).toFixed(2)}
+        </Typography>
+        <TableContainer>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>{product.name}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Description</TableCell>
+                <TableCell>{product.description}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Type</TableCell>
+                <TableCell>{product.type}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Brand</TableCell>
+                <TableCell>{product.brand}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Quantity in stock</TableCell>
+                <TableCell>{product.quantityInStock}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid2>
+    </Grid2>
+  );
 }
