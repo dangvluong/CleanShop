2aae74497a964d05a8b979a05f249913e919703e
Luong Dang <dangvluong@gmail.com>
2025-08-09 20:27:07 +0700

    Add checkout page and fix minor error

--- src/CleanShop.Web/ReactApp/src/app/router/Routes.tsx
+++ src/CleanShop.Web/ReactApp/src/app/router/Routes.tsx
@@ -8,6 +8,7 @@ import ContactPage from '../../features/contact/ContactPage';
 import ServerError from '../errors/ServerError';
 import NotFound from '../errors/NotFound';
 import BasketPage from '../../features/basket/BasketPage';
+import CheckoutPage from '../../features/checkout/CheckoutPage';
 
 export const router = createBrowserRouter([
   {
@@ -19,6 +20,7 @@ export const router = createBrowserRouter([
       { path: 'catalog/:id', element: <ProductDetails /> },
       { path: 'about', element: <AboutPage /> },
       { path: 'basket', element: <BasketPage /> },
+      { path: 'checkout', element: <CheckoutPage /> },
       { path: 'contact', element: <ContactPage /> },
       { path: '/server-error', element: <ServerError /> },
       { path: '/not-found', element: <NotFound /> },
--- src/CleanShop.Web/ReactApp/src/shared/components/OrderSummary.tsx
+++ src/CleanShop.Web/ReactApp/src/shared/components/OrderSummary.tsx
@@ -1,6 +1,7 @@
 import { Box, Typography, Divider, Button, TextField, Paper } from '@mui/material';
 import { currencyFormat } from '../../lib/util';
 import { Basket, Item } from '../../app/models/basket';
+import { Link } from 'react-router';
 
 interface Props {
   basket: Basket;
@@ -43,10 +44,12 @@ export default function OrderSummary({ basket }: Props) {
         </Box>
 
         <Box mt={2}>
-          <Button variant="contained" color="primary" fullWidth sx={{ mb: 1 }}>
+          <Button component={Link} to="/checkout" variant="contained" color="primary" fullWidth sx={{ mb: 1 }}>
             Checkout
           </Button>
-          <Button fullWidth>Continue Shopping</Button>
+          <Button component={Link} to="/catalog" fullWidth>
+            Continue Shopping
+          </Button>
         </Box>
       </Paper>
 
