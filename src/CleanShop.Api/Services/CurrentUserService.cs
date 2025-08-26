b0bafb32e907a456c59d1e646113ea7dcf5ea1a2
Luong Dang <dangvluong@gmail.com>
2025-08-26 20:52:18 +0700

    Fix issue current user can be null

--- src/CleanShop.Api/Services/CurrentUserService.cs
+++ src/CleanShop.Api/Services/CurrentUserService.cs
@@ -4,10 +4,17 @@ using Microsoft.AspNetCore.Identity;
 
 namespace CleanShop.Api.Services
 {
-    public class CurrentUserService(IHttpContextAccessor httpContextAccessor, UserManager<User> userManager) : ICurrentUserService
+    public class CurrentUserService(IHttpContextAccessor httpContextAccessor, UserManager<User> userManager)
+        : ICurrentUserService
     {
-        public string? UserId => userManager.GetUserId(httpContextAccessor.HttpContext?.User);
-        
+        public string? UserId
+        {
+            get
+            {
+                return httpContextAccessor.HttpContext?.User != null ? userManager.GetUserId(httpContextAccessor.HttpContext?.User) : null;
+            }
+        }
+
         public bool IsAuthenticated => httpContextAccessor.HttpContext?.User?.Identity?.IsAuthenticated ?? false;
     }
 }
